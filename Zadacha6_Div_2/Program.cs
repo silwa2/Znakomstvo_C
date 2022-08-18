0f3eb3c29cd126be79c6d49c8a038af4e8ae5fd2
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-18 22:35:39 +0300

    Zadacha 6 Div na 2

---
+++ Zadacha6_Div_2/Program.cs
@@ -0,0 +1,33 @@
+﻿int a = 4;
+
+if (a % 2 == 0)
+{
+Console.WriteLine ("Yes");
+}
+else
+{
+ Console.WriteLine ("No");
+}
+
+a = -3;
+
+if (a % 2 == 0)
+{
+Console.WriteLine ("Yes");
+}
+else
+{
+ Console.WriteLine ("No");
+}
+
+
+a = 7;
+
+if (a % 2 == 0)
+{
+Console.WriteLine ("Yes");
+}
+else
+{
+ Console.WriteLine ("No");
+}
