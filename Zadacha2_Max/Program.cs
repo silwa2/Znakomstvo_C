53c4e34f33f2693b40febd4b74d0df4573b776a2
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-18 21:16:05 +0300

    Zadacha 2 Max

---
+++ Zadacha2_Max/Program.cs
@@ -0,0 +1,37 @@
+﻿int a = 5, b = 7, max;
+
+if(a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.WriteLine(max);
+
+ a = 2;
+ b = 10;
+
+if(a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.WriteLine(max);
+
+ a = -9;
+ b = -3;
+
+if(a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.WriteLine(max);
