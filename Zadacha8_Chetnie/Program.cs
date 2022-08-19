b9e3765314b23d6263c78d860b2958a259d380b3
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-19 08:41:29 +0300

    Zadacha 8 Chetnie

---
+++ Zadacha8_Chetnie/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int number = 1;
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+while (number <= n)
+{if (number % 2 == 0)
+    {
+        Console.Write(number);
+        Console.Write(", ");
+    }
+        number = number + 1;
+}
+
