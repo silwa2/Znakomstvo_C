87e5233d498ab50711009e62f8651012934c158a
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-19 12:25:25 +0300

    Нахождение максимального из трех числел

--- Zadacha4_max2/Program.cs
+++ Zadacha4_max2/Program.cs
@@ -1,26 +1,16 @@
-﻿int a = 2, b = 3, c = 7, max = a;
+﻿
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
 
-if (b > c) max = b;
-if (c > a) max = c;
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
 
-Console.WriteLine (max);
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
 
-a = 44;
-b = 5;
-c = 78;
-max = a;
+if (b > max) max = b;
+if (c > max) max = c;
 
-if (b > c) max = b;
-if (c > a) max = c;
-
-Console.WriteLine (max);
-
-a = 22;
-b = 3;
-c = 9;
-max = a;
-
-if (b > c) max = b;
-if (c > a) max = c;
-
+Console.Write("Максимальное число: ");
 Console.WriteLine (max);
