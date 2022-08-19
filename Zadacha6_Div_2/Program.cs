5791863bd884cd4f464e7f31d934d6947214c889
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-19 13:06:45 +0300

    Деление числа с остатком!

--- Zadacha6_Div_2/Program.cs
+++ Zadacha6_Div_2/Program.cs
@@ -4,11 +4,11 @@ int number = int.Parse (Console.ReadLine());
 
 if (number % 2 == 0)
 {
-Console.Write ("Число делится на 2: ");
-Console.Write ("Yes");
+ Console.Write (("Число ") + number + (" делится на 2:"));
+ Console.WriteLine (" остаток = " + (number % 2));
 }
 else
 {
- Console.Write ("Число делится на 2: ");
- Console.WriteLine ("No");
+ Console.Write (("Число ") + number + (" не делится на 2:"));
+ Console.WriteLine (" остаток = " + (number % 2));
 }
