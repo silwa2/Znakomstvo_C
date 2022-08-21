6331c4c14c22dc51f0c3df9df9344ce47f02d6c2
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-21 17:34:59 +0300

    Вывод большего числа

---
+++ massiv/Zad_1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write ("Введите первое число: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write ("Большее число: " + number1);
+}
+else
+{
+    if (number2 > number1)
+    {
+        Console.Write ("Большее число: " + number2);
+    }
+    else
+    {
+     Console.Write ("Числа равны: " + number1);   
+    }
+}
