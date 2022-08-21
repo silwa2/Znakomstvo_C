95c0df1246bf13b31b05f255a3bd8df05b467f37
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-21 18:17:59 +0300

    Проценты банка

---
+++ massiv/Zad_2_Bank/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write ("Введите сумму вклада: ");
+Double summa = Convert.ToDouble (Console.ReadLine ());
+
+if (summa < 100)
+{
+    summa = summa + (summa * 5 / 100);
+    Console.Write ("Ваш вклад составит: " + summa);
+}
+else
+{
+    if (summa >= 100 && summa < 200)
+    {
+        summa = summa + (summa * 7 / 100);
+        Console.Write ("Ваш вклад составит: " + summa);
+    }
+    else
+    {
+        summa = summa + (summa * 10 / 100);
+        Console.Write ("Ваш вклад составит: " + summa);
+    }
+}
