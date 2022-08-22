21a50dbc76f84cce68c476b4ccf910b2d266c4e6
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-22 19:45:43 +0300

    Вывод третьей цифры

---
+++ Seminar_2_Arrey/Zadacha_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write ("Введите число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string numbersText = Convert.ToString(numbers);
+
+if (numbersText.Length > 2)
+{
+    Console.Write("Третья цифра числа: " + numbersText[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры в этом числе нет.");
+}
