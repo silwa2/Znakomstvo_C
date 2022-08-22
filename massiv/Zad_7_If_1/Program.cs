8f6211ad1c20497ef5f6db61badb9ac9e28ba2c8
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-22 14:36:49 +0300

    Вывод чмсла на экран

---
+++ massiv/Zad_7_If_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// C1.2. Составить программу вывода любого числа любое заданное число раз 
+
+Console.Write("Введите число вывода на экран: ");
+int number = int.Parse (Console.ReadLine());
+
+Console.Write("Введите количество раз вывода на экран: ");
+int numberRaz = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while (index < numberRaz)
+{
+    Console.Write(number + " ");
+    index ++;
+}
