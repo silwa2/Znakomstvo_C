caee232b9481e313fd2abbe62ef8d295f884697c
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-22 14:58:48 +0300

    Вывести числа следующим образом

---
+++ massiv/Zad_8/Program.cs
@@ -0,0 +1,9 @@
+﻿//C1.4
+            Console.WriteLine("C1.4. Напечатать числа следующим образом:");
+            for (int a = 10; a < 26; a++) Console.WriteLine($"{a} {a}.04");
+            Console.ReadKey();
+            Console.Clear();
+            for (double b = 24.8; b < 35; b += 1) Console.WriteLine($"{(int) b + 1} {b+0.7} {b}");
+            
+
+            
