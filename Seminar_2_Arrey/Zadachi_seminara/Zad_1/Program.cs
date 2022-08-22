1f1c79fb2f46ecaf8981dd2805da3f0929cd6081
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-22 20:44:46 +0300

    Max

---
+++ Seminar_2_Arrey/Zadachi_seminara/Zad_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа
+
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+
+int num1 = num / 10;
+int num2 = num % 10;
+if (num1 > num2)
+{
+Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
+
