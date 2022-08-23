3547023208800570c72db8d39ca257d9e34c19ed
Sinkov Aleksandr <silwa2@yandex.ru>
2022-08-23 11:18:38 +0300

    Задача 10

--- Seminar_2_Arrey/Zadacha_10/Program.cs
+++ Seminar_2_Arrey/Zadacha_10/Program.cs
@@ -1,18 +1,33 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход 
 // трёхзначное число и на выходе показывает вторую цифру этого числа
 
-Fri:
-
+// Первое решение
 Console.Write("Введите трехзначное число: ");
 int numbers = Convert.ToInt32(Console.ReadLine());
 
 if (numbers > 99 && numbers < 1000)
 {
-string strNumbers = Convert.ToString(numbers);
-Console.WriteLine("Вторая цифра этого числа: " + strNumbers[1]);
+    int number1 = (numbers % 100) / 10;
+    Console.WriteLine(number1);
 }
 else
 {
-Console.WriteLine ("Вы ввели неверное число, введите заного.");
-goto Fri;
+    Console.WriteLine ("Вы ввели неправильное число.");
 }
+
+//второе решение
+// Fri:
+
+// Console.Write("Введите трехзначное число: ");
+// int numbers = Convert.ToInt32(Console.ReadLine());
+
+// if (numbers > 99 && numbers < 1000)
+// {
+// string strNumbers = Convert.ToString(numbers);
+// Console.WriteLine("Вторая цифра этого числа: " + strNumbers[1]);
+// }
+// else
+// {
+// Console.WriteLine ("Вы ввели неверное число, введите заного.");
+// goto Fri;
+// }
