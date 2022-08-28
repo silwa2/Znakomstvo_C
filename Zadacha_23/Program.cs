9e2b9fe4db2dee792884859d80d3094886f630fc
Sinkov <silwa2@yandex.ru>
2022-08-28 13:09:33 +0300

    Вывод кубов чисел

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+double result;
+
+for (result = 1; result <= number; result++)
+{
+    Console.Write(" " + Math.Pow(result, 3));
+}
+Console.ReadKey();
