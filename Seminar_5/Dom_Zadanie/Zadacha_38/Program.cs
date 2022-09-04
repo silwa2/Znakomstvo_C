99243a231fb884f4ad4d7247ca6beb35ead57b72
Sinkov <silwa2@yandex.ru>
2022-09-04 18:32:34 +0300

    Задача 38 разница между элементами

---
+++ Seminar_5/Dom_Zadanie/Zadacha_38/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int [] array = new int [5];
+int max = 0, min = 110, numArray = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    numArray = new Random().Next(0,100);
+    Console.Write(" " + numArray);
+    
+    if (numArray > max)
+    {
+        max = numArray;
+    }
+    if (numArray < min)
+    {
+        min = numArray;
+    }
+}
+Console.Write ("->" + (max - min));
+Console.ReadKey();
+
+
