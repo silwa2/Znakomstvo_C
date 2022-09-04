4d90a7a14a82832473e023db2a4728e9d0ecce93
Sinkov <silwa2@yandex.ru>
2022-09-04 17:06:09 +0300

    Задача 34 Задать массив

---
+++ Seminar_5/Dom_Zadanie/Zadacha_34/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int [] array = new int [10];
+int positivNum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+int num = new Random().Next(100, 1000);
+Console.Write(" " + num);
+if (num%2 == 0)
+    {
+        positivNum ++;
+    }
+    
+}
+
+Console.Write ("->" + positivNum);
+Console.ReadKey();
