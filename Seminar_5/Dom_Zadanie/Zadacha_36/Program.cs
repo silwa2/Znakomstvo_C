9980760f3a01e78277e2e6fa744604e9342609db
Sinkov <silwa2@yandex.ru>
2022-09-04 17:43:09 +0300

    Задача 36 Сумма нечетных элементов массива

---
+++ Seminar_5/Dom_Zadanie/Zadacha_36/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 36: Задайте одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] array = new int [10];
+int posiziya = 0, numArray = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    numArray = new Random().Next(0, 10);
+    Console.Write(" " + numArray);
+
+    if (i%2 == 1)
+    {
+     posiziya += numArray;
+    }
+}
+Console.Write ("->" + posiziya);
+Console.ReadKey();
