6ff4ca2d0d74c8a00288b0309a7e3f582630d792
Sinkov <silwa2@yandex.ru>
2022-09-14 19:56:32 +0300

    Массив с наименьшей суммой строки

--- Seminar_8/Zadacha_56/Program.cs
+++ Seminar_8/Zadacha_56/Program.cs
@@ -11,44 +11,17 @@
 // Программа считает сумму элементов в каждой строке 
 // и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-// Console.WriteLine("Первоначальный массив: ");
-// //int [,] matrix = new int[new Random().Next(1,10), new Random().Next(1,10)];
-// int [,] matrix = new int[3,3];
-// for (int i = 0; i < matrix.GetLength(0); i++)
-// {
-//     for (int j = 0; j < matrix.GetLength(1); j++)
-//     {
-//         matrix[i,j] = new Random().Next(1,10);
-//         Console.Write(matrix[i,j] + "\t");
-//     }
-//     Console.WriteLine();
 
-int[,] CreateMatrix (int row, int col, int min, int max) 
+int [,] matrix = new int[new Random().Next(1,10), new Random().Next(1,10)];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    int[,] matrix = new int[row, col];
-    Random rnd = new Random();   
-    for (int i = 0; i < matrix.GetLength(0); i++) 
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i,j] = rnd.Next(min, max +1);
-        }
+        matrix[i,j] = new Random().Next(1,10);
+        Console.Write(matrix[i,j] + "\t");
     }
-    return matrix;
-} 
-
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i <  matrix.GetLength(0); i++)
-    {
-        Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++) 
-        {
-            if (j < matrix.GetLength(1) -1) Console.Write($"{matrix[i, j], 1}|");
-            else Console.Write($"{matrix[i, j], 1}");
-        }
-        Console.WriteLine("|");        
-    }        
+    Console.WriteLine();
 }
 
 static void SumStringMatrix(int[,] matrix)
@@ -72,14 +45,7 @@ static void SumStringMatrix(int[,] matrix)
             index = i;
         }
     }
-    string line = string.Empty;
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        line += matrix[index, j] + " ";
-    }
-    Console.WriteLine($"Строка с минимальной суммой элементов равна {line}. ");
+    Console.WriteLine($"Строка с минимальной суммой элементов равна {index+1}. ");
 }
 
-int[,] array2D = CreateMatrix(3, 4, 0, 9);
-PrintMatrix(array2D);
-SumStringMatrix(array2D);
+SumStringMatrix(matrix);
